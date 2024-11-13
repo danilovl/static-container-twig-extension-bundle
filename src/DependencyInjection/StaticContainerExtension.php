173a908e5433235bd143314cca7252c2d140f61a
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 20:54:06 +0100

    StaticContainerExtension - change Extension class.

--- src/DependencyInjection/StaticContainerExtension.php
+++ src/DependencyInjection/StaticContainerExtension.php
@@ -3,9 +3,9 @@
 namespace Danilovl\StaticContainerTwigExtensionBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class StaticContainerExtension extends Extension
 {
